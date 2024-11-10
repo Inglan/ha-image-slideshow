3d15fa034b94123a92c283cf3a3d1785921fba5f
Ingo <me@ingowolf.au>
2024-11-10 09:00:40 +0000

    add init code

--- __init__.py
+++ __init__.py
@@ -1,35 +1,54 @@
-"""The Folder Image Slideshow integration."""
+"""Camera platform for Folder Image Slideshow."""
 
 from __future__ import annotations
 
+import os
+import random
+
+from homeassistant.components.camera import Camera
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
+from homeassistant.const import CONF_FILE_PATH
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.event import async_track_time_interval
+from datetime import timedelta
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
-PLATFORMS: list[Platform] = [Platform.LIGHT]
-
-# TODO Create ConfigEntry type alias with API object
-# TODO Rename type alias and update all entry annotations
-type New_NameConfigEntry = ConfigEntry[MyApi]  # noqa: F821
+from .const import DOMAIN
 
 
-# TODO Update entry annotation
-async def async_setup_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> bool:
-    """Set up Folder Image Slideshow from a config entry."""
-
-    # TODO 1. Create API instance
-    # TODO 2. Validate the API connection (and authentication)
-    # TODO 3. Store an API object for your platforms to access
-    # entry.runtime_data = MyAPI(...)
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
-    return True
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities
+):
+    """Set up the Folder Image Slideshow Camera."""
+    directory = hass.data[DOMAIN][entry.entry_id]["directory"]
+    camera = SlideshowCamera(directory)
+    async_add_entities([camera])
 
 
-# TODO Update entry annotation
-async def async_unload_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> bool:
-    """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+class SlideshowCamera(Camera):
+    """Representation of a Folder Image Slideshow Camera."""
+
+    def __init__(self, directory: str) -> None:
+        """Initialize the camera."""
+        super().__init__()
+        self.directory = directory
+        self._last_image = None
+        self.update_interval = timedelta(minutes=1)
+        self._update_image()
+        async_track_time_interval(self.hass, self._update_image, self.update_interval)
+
+    def camera_image(self):
+        """Return the image for the camera."""
+        return self._last_image
+
+    def _update_image(self, now=None):
+        """Update the image with a random image from the directory."""
+        files = [
+            os.path.join(self.directory, f)
+            for f in os.listdir(self.directory)
+            if os.path.isfile(os.path.join(self.directory, f))
+        ]
+        if files:
+            image_path = random.choice(files)
+            with open(image_path, "rb") as img_file:
+                self._last_image = img_file.read()
+            self.async_write_ha_state()
